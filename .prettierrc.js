b47a8761bb999dfb99a2b3f37030f8fdb5728238
Brick Clinton <102443870+BrickClinton@users.noreply.github.com>
2023-07-29 20:57:27 -0500

    Incorporando nuevos modulos

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     trailingComma: 'none',
-    tabWidth: 4,
+    tabWidth: 2,
     singleQuote: true,
     printWidth: 80,
     semi: true,
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,18 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+import { environment } from 'config/config';
+
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
+    private config = environment;
     constructor(private http: HttpClient) {}
+
+    private get baseUrl(){
+        return this.config.apiKey + '';
+    }
+
+
 }
